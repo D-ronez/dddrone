3499fb86f189610e350882c9311d3ae016668d0c
DmitryMurashov <dmtr.murashov@gmail.com>
2024-11-09 15:36:29 +0300

    [ cmake] Build | Compile definitions from configs

--- cmake/BuildCommon.cmake
+++ cmake/BuildCommon.cmake
@@ -1,11 +1,13 @@
+# This most come before Config.cmake The common library for all the targets
+add_library(${PROJECT_NAME}Common INTERFACE)
+include(Config)
+
 ddd_config(DDD_OPENGL_INCLUDE_DIRS "OpenGL include directories" "")
 ddd_config(DDD_OPENGL_SHARED_LIB_DIR "" "")
 
-# Find OpenGL library
+# Find OpenGL libraries
 find_library(GL GL PATHS ${DDD_OPEGL_SHARED_LIB_DIR} REQUIRED)
 find_library(GLU GLU REQUIRED)
 find_library(glut glut REQUIRED)
-
-add_library(${PROJECT_DIR}Common INTERFACE)
-target_link_libraries(${PROJECT_DIR}Common INTERFACE GL GLU glut)
-target_include_directories(${PROJECT_DIR}Common INTERFACE src/ ${DDD_OPENGL_INCLUDE_DIRS})
+target_link_libraries(${PROJECT_NAME}Common INTERFACE GL GLU glut)
+target_include_directories(${PROJECT_NAME}Common INTERFACE src/ ${DDD_OPENGL_INCLUDE_DIRS})
--- cmake/BuildExamples.cmake
+++ cmake/BuildExamples.cmake
@@ -2,5 +2,5 @@ ddd_config(DDD_BUILD_EXAMPLES "Build examples" "1")
 
 if(NOT "${DDD_BUILD_EXAMPLES}" STREQUAL "")
 	add_executable(Triangle examples/triangle.c)
-	target_link_libraries(Triangle PRIVATE ${PROJECT_DIR}Common)
+	target_link_libraries(Triangle PRIVATE ${PROJECT_NAME}Common)
 endif()
